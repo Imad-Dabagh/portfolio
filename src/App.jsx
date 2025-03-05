6dff9173bcd6b5fd8a392361d9352f6c89074c47
Imad-Dabagh <imaddabagh@gmail.com>
2025-03-05 22:37:23 +0000

    Separated Experience and Skills components, added new assets, and updated history and skills data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
+import { Skills } from "./components/Skills/Skills";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Skills />
       <Projects />
       <Contact />
     </div>
